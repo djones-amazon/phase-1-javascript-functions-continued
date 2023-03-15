8fd4367e0a9273a8911b7506d43071168d404942
Devin Jones <devin.jones21083@gmail.com>
2023-03-15 13:37:47 -0400

    Edited index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun (someActivity) {
+    if (someActivity) {
+    } else {
+        someActivity = `roller-skate`;
+    }
+    return (`This Saturday, I want to ${someActivity}!`);
+}
+
+function mondayWork (someActivity) {
+    if (someActivity) {
+    } else {
+        someActivity = `go to the office`;
+    }
+    return (`This Monday, I will ${someActivity}.`);
+}
+
+function wrapAdjective (txtWrapper) {
+    return function (someAdj) {
+        if (someAdj) {
+        } else {
+            someAdj = `Special`;
+        }
+        return (`You are ${txtWrapper}${someAdj}${txtWrapper}!`);
+    }
+}
